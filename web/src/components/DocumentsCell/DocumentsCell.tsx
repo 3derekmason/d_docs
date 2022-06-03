3a11c0cbd5c347871c743569d5d989bf35eeab3d
Derek Mason <3derekmason@gmail.com>
2022-06-03 12:19:50 -0600

    add documents cell to home page

---
+++ web/src/components/DocumentsCell/DocumentsCell.tsx
@@ -0,0 +1,29 @@
+import type { DocumentsQuery } from 'types/graphql'
+import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+
+
+export const QUERY = gql`
+  query DocumentsQuery {
+    documents {
+      id
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }: CellFailureProps) => (
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
+)
+
+export const Success = ({ documents }: CellSuccessProps<DocumentsQuery>) => {
+  return (
+    <ul>
+      {documents.map((item) => {
+        return <li key={item.id}>{JSON.stringify(item)}</li>
+      })}
+    </ul>
+  )
+}
