514db01c7c2549b8e2258ee24afaa87b7f0af86d
Derek Mason <3derekmason@gmail.com>
2022-06-04 09:35:18 -0600

    footer init

---
+++ web/src/components/AppFooter/AppFooter.tsx
@@ -0,0 +1,10 @@
+const AppFooter = () => {
+  return (
+    <div>
+      <h2>{'AppFooter'}</h2>
+      <p>{'Find me in ./web/src/components/AppFooter/AppFooter.tsx'}</p>
+    </div>
+  )
+}
+
+export default AppFooter
