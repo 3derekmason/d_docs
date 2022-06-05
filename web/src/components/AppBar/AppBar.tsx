ceb2ba4ccdd456d7a42044db77f38967ba588410
Derek Mason <3derekmason@gmail.com>
2022-06-05 08:59:32 -0600

    logout init in appbar

--- web/src/components/AppBar/AppBar.tsx
+++ web/src/components/AppBar/AppBar.tsx
@@ -1,11 +1,23 @@
 import { Link, routes } from '@redwoodjs/router'
 import styles from '/src/components/AppBar/appbar.module.css'
+import { useAuth } from '@redwoodjs/auth'
+
 const AppBar = () => {
+  const { isAuthenticated, currentUser, logOut } = useAuth()
   return (
     <header className={styles.appBar}>
       <h1>
         <Link to={routes.documents()}>d_docs</Link>
       </h1>
+      {isAuthenticated ? (
+        <div>
+          <button type="button" onClick={logOut}>
+            Logout
+          </button>
+        </div>
+      ) : (
+        <Link to={routes.login()}>Login</Link>
+      )}
       <nav>
         <ul className={styles.navBar}>
           <li>
