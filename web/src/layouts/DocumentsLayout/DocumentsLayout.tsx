02c10a3d9038dc142be7ba356ef8a2da12f8ee6c
Derek Mason <3derekmason@gmail.com>
2022-06-05 09:23:50 -0600

    edit admin page

--- web/src/layouts/DocumentsLayout/DocumentsLayout.tsx
+++ web/src/layouts/DocumentsLayout/DocumentsLayout.tsx
@@ -1,29 +1,29 @@
 import { Link, routes } from '@redwoodjs/router'
 import { Toaster } from '@redwoodjs/web/toast'
-
+import AppBar from 'src/components/AppBar'
 type DocumentLayoutProps = {
   children: React.ReactNode
 }
 
 const DocumentsLayout = ({ children }: DocumentLayoutProps) => {
   return (
-    <div className="rw-scaffold">
-      <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
-      <header className="rw-header">
-        <h1 className="rw-heading rw-heading-primary">
-          <Link to={routes.documents()} className="rw-link">
-            Documents
+    <>
+      <AppBar />
+      <div className="rw-scaffold">
+        <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
+        <header className="rw-header">
+          <h1 className="rw-heading rw-heading-primary">
+            <Link to={routes.documents()} className="rw-link">
+              Documents
+            </Link>
+          </h1>
+          <Link to={routes.newDocument()} className="rw-button rw-button-green">
+            <div className="rw-button-icon">+</div> New Document
           </Link>
-        </h1>
-        <Link to={routes.newDocument()} className="rw-button rw-button-green">
-          <div className="rw-button-icon">+</div> New Document
-        </Link>
-        <Link to={'/'}>
-          <button>HOME</button>
-        </Link>
-      </header>
-      <main className="rw-main">{children}</main>
-    </div>
+        </header>
+        <main className="rw-main">{children}</main>
+      </div>
+    </>
   )
 }
 
