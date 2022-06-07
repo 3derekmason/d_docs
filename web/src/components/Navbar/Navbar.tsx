471c4bb78d030f1688ad1610d141ee0857a19e09
Derek Mason <3derekmason@gmail.com>
2022-06-07 16:25:05 -0600

    navbar animation init

--- web/src/components/Navbar/Navbar.tsx
+++ web/src/components/Navbar/Navbar.tsx
@@ -3,12 +3,19 @@ import styles from 'src/components/Navbar/navbar.module.css'
 
 const Navbar = () => {
   const [open, setOpen] = useState(false)
+
   const toggleOpen = () => {
     setOpen(!open)
   }
+
   return (
     <div className={styles.navbar}>
       <button onClick={toggleOpen}>{open ? 'Close' : 'Open'}</button>
+      <div className={styles.navbarContainer}>
+        <div className={open ? styles.openBar : styles.closedBar}>
+          Link/whatever
+        </div>
+      </div>
     </div>
   )
 }
