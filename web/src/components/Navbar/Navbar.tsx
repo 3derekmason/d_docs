a0adb7dc8e29f9f84173ba2bab52b1055b8d95ed
Derek Mason <3derekmason@gmail.com>
2022-06-07 15:59:03 -0600

    open/close toggle init

--- web/src/components/Navbar/Navbar.tsx
+++ web/src/components/Navbar/Navbar.tsx
@@ -1,9 +1,14 @@
+import { useState } from 'react'
 import styles from 'src/components/Navbar/navbar.module.css'
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false)
+  const toggleOpen = () => {
+    setOpen(!open)
+  }
   return (
     <div className={styles.navbar}>
-      <h2>{'Navbar'}</h2>
+      <button onClick={toggleOpen}>{open ? 'Close' : 'Open'}</button>
     </div>
   )
 }
