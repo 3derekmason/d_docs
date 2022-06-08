d27a03cc6ee9b43ba64ea4ba9928b939ad6e0101
Derek Mason <3derekmason@gmail.com>
2022-06-08 16:09:38 -0600

    edit about page

--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -1,20 +1,42 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
-
+import styles from 'src/pages/AboutPage/aboutPage.module.css'
 const AboutPage = () => {
   return (
-    <>
+    <div className={styles.aboutPage}>
       <MetaTags title="About" description="About page" />
-
-      <h1>AboutPage</h1>
+      <h1 style={{ color: '#D1C4E9', borderBottom: '0.5px solid #ffffff80' }}>
+        DDocumentation
+      </h1>
       <p>
-        Find me in <code>./web/src/pages/AboutPage/AboutPage.tsx</code>
+        DDocumentation or <em style={{ color: '#D1C4E9' }}>d_docs</em> is the
+        source of information for open source packages published by Derek Mason.
+      </p>
+      <h3 style={{ color: '#7C4DFF' }}>Getting Started</h3>
+      <p>
+        Select a package from the home page to view details. Available
+        information includes
+        <ul>
+          <li>Package version</li>
+          <li>Unpackaged size</li>
+          <li>Download link</li>
+          <li>Source code link</li>
+        </ul>
       </p>
       <p>
-        My default route is named <code>about</code>, link to me with `
-        <Link to={routes.about()}>About</Link>`
+        To leave a comment or a message, choose the{' '}
+        <strong style={{ color: '#D1C4E9' }}>Contact</strong> option from the
+        navigation menu.
       </p>
-    </>
+      <h4 style={{ color: '#7C4DFF' }}>Contributors</h4>
+      <p>
+        Derek Mason is a developer in Denver, Colorado who uses software
+        engineering as a tool to bring impactful ideas to life. Find out more at{' '}
+        <a href="https://derekmason.dev" target="_blank" rel="noreferrer">
+          derekmason.dev
+        </a>
+      </p>
+    </div>
   )
 }
 
