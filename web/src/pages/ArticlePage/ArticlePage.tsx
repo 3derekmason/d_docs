ed3c5978bc0675f3e2655580bfdd91c4ac5a9ec8
Derek Mason <3derekmason@gmail.com>
2022-06-07 18:27:53 -0600

    add navbar to article / document page

--- web/src/pages/ArticlePage/ArticlePage.tsx
+++ web/src/pages/ArticlePage/ArticlePage.tsx
@@ -1,6 +1,7 @@
 // import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 import DocumentCell from 'src/components/Document/DocumentCell'
+import Navbar from 'src/components/Navbar/Navbar'
 
 interface Props {
   id: number
@@ -8,10 +9,16 @@ interface Props {
 
 const ArticlePage = ({ id }: Props) => {
   return (
-    <>
+    <div
+      style={{
+        height: '100%',
+        display: 'flex',
+      }}
+    >
       <MetaTags title="Article" description="Article page" />
+      <Navbar />
       <DocumentCell id={id} />
-    </>
+    </div>
   )
 }
 
