59d760f8adb2151686bfd4ebfe8de29f9f16f3fe
Derek Mason <3derekmason@gmail.com>
2022-06-08 13:15:47 -0600

    login / admin added to navbar

--- web/src/components/Navbar/Navbar.tsx
+++ web/src/components/Navbar/Navbar.tsx
@@ -1,12 +1,50 @@
 import { useState } from 'react'
 import styles from 'src/components/Navbar/navbar.module.css'
 import { Link, routes, navigate } from '@redwoodjs/router'
-const bars = [
-  { route: routes.contact(), name: 'Contact' },
-  { route: routes.about(), name: 'About' },
-]
+import { useAuth } from '@redwoodjs/auth'
+
+// {isAuthenticated ? (
+//   <div>
+//     <a href="" onClick={logOut}>
+//       Logout
+//     </a>
+//   </div>
+// ) : (
+//   <Link to={routes.login()}>Login</Link>
+// )}
 
 const Navbar = () => {
+  const { isAuthenticated, logOut } = useAuth()
+  const bars = [
+    {
+      route: () => {
+        navigate(routes.contact())
+      },
+      name: 'Contact',
+    },
+    {
+      route: () => {
+        navigate(routes.about())
+      },
+      name: 'About',
+    },
+    isAuthenticated
+      ? {
+          route: () => {
+            navigate(routes.documents())
+          },
+          name: 'Admin',
+        }
+      : '',
+    {
+      route: isAuthenticated
+        ? logOut
+        : () => {
+            navigate(routes.login())
+          },
+      name: isAuthenticated ? 'Logout' : 'Login',
+    },
+  ]
   const [open, setOpen] = useState(true)
 
   const toggleOpen = () => {
@@ -20,13 +58,11 @@ const Navbar = () => {
         {bars.map((bar, i) => {
           return (
             <div
-              onClick={() => {
-                navigate(bar.route)
-              }}
+              onClick={bar.route}
               key={i}
               className={open ? styles.openBar : styles.closedBar}
             >
-              <Link to={bar.route}>{bar.name}</Link>
+              {bar.name}
             </div>
           )
         })}
