99218102fdc4e9fd8fca960e1aac550dafda5091
Derek Mason <3derekmason@gmail.com>
2022-06-09 09:38:40 -0600

    prevelant comments

--- web/src/pages/Document/DocumentsPage/DocumentsPage.tsx
+++ web/src/pages/Document/DocumentsPage/DocumentsPage.tsx
@@ -4,9 +4,9 @@ import styles from 'src/pages/Document/DocumentsPage/documentsPage.module.css'
 const DocumentsPage = () => {
   return (
     <div className={styles.documentsPage}>
-      <DocumentsCell />
       <h3 style={{ margin: 0, color: '#00838F' }}>Comments/Messages</h3>
       <ContactsCell />
+      <DocumentsCell />
     </div>
   )
 }
